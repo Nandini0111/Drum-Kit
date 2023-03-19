101b980005179fa253d7823665a31fd9079072ce
Nandini0111 <97455242+Nandini0111@users.noreply.github.com>
2023-03-19 16:23:54 +0530

    Add files via upload

---
+++ drumkit.js
@@ -0,0 +1,48 @@
+
+for (let index = 0; index < document.querySelectorAll("button").length; index++) {
+    document.querySelectorAll("button")[index].addEventListener("click", function(){
+        var buttonInnerHTML=this.innerHTML;
+        makeSound(buttonInnerHTML);
+       
+    });
+
+    document.addEventListener("keydown", function(event){
+        makeSound(event.key);
+    })
+    function makeSound(key){
+        switch (key) {
+            case "w":
+                var tom_1= new Audio("sounds/tom-1.mp3");
+                tom_1.play();
+                break;
+            case "a":
+                var tom_2= new Audio("sounds/tom-2.mp3");
+                tom_2.play();
+                break;
+            case "s":
+                var tom_3= new Audio("sounds/tom-3.mp3");
+                tom_3.play();
+                break;
+            case "d":
+                var tom_4= new Audio("sounds/tom-4.mp3");
+                tom_4.play();
+                break;
+            case "j":
+                var snare= new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+            case "k":
+                var crash= new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+            case "l":
+                var kick= new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
+
